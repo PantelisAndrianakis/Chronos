acb9df73f53795143da035b821a47629df3116a4
PantelisAndrianakis <pantelisandrianakis@gmail.com>
2021-02-03 18:46:22 +0200

    Addition of package name.

--- Chronos.java
+++ Chronos.java
@@ -19,6 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package chronos;
 
 /**
  * Chronos updates an internal long value with System.currentTimeMillis() every 1 millisecond.<br>
